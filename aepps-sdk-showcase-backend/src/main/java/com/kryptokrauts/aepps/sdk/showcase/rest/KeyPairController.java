6ad6dc0a0f607215f874b8d1a15224df15af8b62
Marco Walz <walz.marcoo@gmail.com>
2019-07-19 00:44:53 +0200

    added generation of mnemonic and recovery of keypairs by privatekey and mnemonic

--- aepps-sdk-showcase-backend/src/main/java/com/kryptokrauts/aepps/sdk/showcase/rest/KeyPairController.java
+++ aepps-sdk-showcase-backend/src/main/java/com/kryptokrauts/aepps/sdk/showcase/rest/KeyPairController.java
@@ -1,24 +1,67 @@
 package com.kryptokrauts.aepps.sdk.showcase.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.kryptokrauts.aeternity.sdk.domain.secret.impl.BaseKeyPair;
+import com.kryptokrauts.aeternity.sdk.domain.secret.impl.MnemonicKeyPair;
+import com.kryptokrauts.aeternity.sdk.exception.AException;
+import com.kryptokrauts.aeternity.sdk.service.keypair.KeyPairService;
+import com.kryptokrauts.aeternity.sdk.service.keypair.KeyPairServiceConfiguration;
 import com.kryptokrauts.aeternity.sdk.service.keypair.KeyPairServiceFactory;
+import com.kryptokrauts.aeternity.sdk.util.EncodingUtils;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/keypair")
 public class KeyPairController {
 
-	@RequestMapping(method = RequestMethod.GET, path = "")
-	public String ping() {
-		return KeyPairController.class.getName() + " is available";
-	}
+  private static KeyPairService keyPairService = new KeyPairServiceFactory().getService();
 
-	@RequestMapping(method = RequestMethod.GET, path = "/generate")
-	public BaseKeyPair generateKeyPair() {
-		return new KeyPairServiceFactory().getService().generateBaseKeyPair();
-	}
+  @GetMapping(path = "")
+  public String ping() {
+    return KeyPairController.class.getName() + " is available";
+  }
 
+  @GetMapping(path = "/generate")
+  public BaseKeyPair generateKeyPair() {
+    return keyPairService.generateBaseKeyPair();
+  }
+
+  @GetMapping(path = "/generateMnemonic")
+  public List<String> generateMnemonic(@RequestParam Optional<Integer> entropyInBytes)
+      throws AException {
+    MnemonicKeyPair mnemonicKeyPair;
+    if (entropyInBytes.isPresent()) {
+      mnemonicKeyPair = new KeyPairServiceFactory().getService(
+          KeyPairServiceConfiguration.configure().entropySizeInByte(entropyInBytes.get()).compile())
+          .generateMasterMnemonicKeyPair(null);
+    } else {
+      mnemonicKeyPair = keyPairService.generateMasterMnemonicKeyPair(null);
+    }
+    return mnemonicKeyPair.getMnemonicSeedWords();
+  }
+
+  @GetMapping(path = "/recover")
+  public BaseKeyPair recoverKeyPairFromPrivateKey(@RequestParam String privateKey) {
+    return keyPairService.generateBaseKeyPairFromSecret(privateKey);
+  }
+
+  @GetMapping(path = "/recoverByMnemonic")
+  public List<BaseKeyPair> recoverKeyPairsFromMnemonic(@RequestParam List<String> mnemonic,
+      @RequestParam int numberOfKeyPairs,
+      @RequestParam Optional<String> password)
+      throws AException {
+    List<BaseKeyPair> baseKeyPairs = new ArrayList<>();
+    MnemonicKeyPair mnemonicKeyPair = keyPairService
+        .recoverMasterMnemonicKeyPair(mnemonic, password.orElse(null));
+    for (int i = 0; i < numberOfKeyPairs; i++) {
+      baseKeyPairs.add(EncodingUtils
+          .createBaseKeyPair(keyPairService.generateDerivedKey(mnemonicKeyPair, true)));
+    }
+    return baseKeyPairs;
+  }
 }
